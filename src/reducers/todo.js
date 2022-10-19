08bc836283a79610cdcad129a9b15a090c01f5bb
HirenLimbasiya <107846821+HirenLimbasiya@users.noreply.github.com>
2022-10-19 11:40:37 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,7 +1,16 @@
 const todoReducer = (state, action) => {
-    switch (action.type) {
+  switch (action.type) {
+    case "Add":
+      return [...state, { title: action.payload.title, id: action.payload.id }];
 
-    }
-}
+    case "Delete":
+      return state.filter((e) => {
+        return e.id !== action.payload.id;
+      });
 
-export { todoReducer }
+    default:
+      return state;
+  }
+};
+
+export { todoReducer };
