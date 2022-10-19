d4d93753ddf24689b0744769880ee7a412d60a61
HirenLimbasiya <107846821+HirenLimbasiya@users.noreply.github.com>
2022-10-19 11:40:09 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,37 @@
-import React from "react"
+import React, { useState } from "react";
 
 const AddTodo = ({ dispatch }) => {
+  const [input, setInput] = useState("");
 
+  function handleInput(e) {
+    setInput(e.target.value);
+  }
 
-    return (
-        <></>
-    )
-}
+  function addTodo(e) {
+    e.preventDefault();
+    dispatch({
+      type: "Add",
+      payload: {
+        title: input,
+        id: Date.now()
+      }
+    });
+  }
 
-export { AddTodo }
+  return (
+    <>
+      <form id="todo-form" onSubmit={addTodo}>
+        <input
+          type="text"
+          placeholder="enter title"
+          id="todo-input"
+          value={input}
+          onChange={handleInput}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </>
+  );
+};
+
+export { AddTodo };
